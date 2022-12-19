38214c4ad01ada0ebddc886ec04429869f6b2eee
yanst <stepuro.yan@gmail.com>
2022-12-19 20:19:15 +0300

    добавили способ решения задачи 2 через math

---
+++ Lesson1/Homework/Task2_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number 1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number 2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = Math.Max(number1, number2);
+int min = Math.Min(number1, number2);
+
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
