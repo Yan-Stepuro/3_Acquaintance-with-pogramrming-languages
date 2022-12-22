70130e93dacdbd0ea22effedbc99834b40e2a466
yanst <stepuro.yan@gmail.com>
2022-12-23 02:13:36 +0300

    Добавили 1 задачу по ДЗ по 2 уроку

---
+++ Lesson2/Homework/Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Enter 3 digit number:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 || number > 999)
+{
+    System.Console.WriteLine("Incorrect number");
+}
+else
+{
+    number = number / 10 % 10;
+}
+
+System.Console.WriteLine(number);
