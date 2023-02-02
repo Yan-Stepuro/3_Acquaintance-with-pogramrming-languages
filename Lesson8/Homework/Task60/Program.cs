4054f0c98365911c5f754d04a4bbee23a7f9c97b
yanst <stepuro.yan@gmail.com>
2023-02-02 21:26:20 +0300

    дополнено домашнее задание к уроку 8/

--- Lesson8/Homework/Task60/Program.cs
+++ Lesson8/Homework/Task60/Program.cs
@@ -60,7 +60,7 @@ void PrintArray(int[,,] array)
 {
     for(int k = 0; k < array.GetLength(2); k++)
     {
-        for(int i = 0; i < array.GetLength(0); i++)
+        for(int i = 0; i < array.GetLength(0); j++)
         {
             for(int j = 0; j < array.GetLength(1); j++)
             {
