8bb97238a2f33df9447bfc4dfc1cf67530e218e4
yanst <stepuro.yan@gmail.com>
2023-02-02 21:23:30 +0300

    дополнено домашнее задание к уроку 8

---
+++ Lesson8/Homework/Task60/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillArrayWithRandomNumbers(int[] array)
+{
+    Random rnd = new Random();
+
+    array[0] = rnd.Next(10, 100);
+
+    for(int i = 1; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(10, 100);
+
+        for(int j = 0; j < i; j++)
+        {
+            while(array[i] == array[j])
+            {
+                array[i] = rnd.Next(10, 100);
+                j = 0;
+            }
+        }
+    }
+
+}
+
+void FillArray(int[,,] array)
+{
+    int[] NumbersToFillArray = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    FillArrayWithRandomNumbers(NumbersToFillArray);
+
+    System.Console.WriteLine("Числа для заполнения массива: " + string.Join(", ", NumbersToFillArray) + "\n");
+
+    int indexNumbersToFillArray = 0; 
+    int number = NumbersToFillArray[indexNumbersToFillArray];
+
+    for(int k = 0; k < array.GetLength(2); k++)
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j, k] = number;
+                indexNumbersToFillArray++;
+
+                if(indexNumbersToFillArray == NumbersToFillArray.Length)
+                    return;
+                number = NumbersToFillArray[indexNumbersToFillArray];
+
+            }
+            
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for(int k = 0; k < array.GetLength(2); k++)
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(string.Format("{0, 13}", array[i, j, k] + $"{(i, j, k)}" + " "));
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
+int rows = 2;
+int columns = 2;
+int z = 2;
+
+int[,,] array = new int[rows, columns, z];
+
+FillArray(array);
+PrintArray(array);
---
+++ Lesson8/Homework/Task62/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+void FillArraySpirally(int[,] array)
+{
+    int digit = 1;
+    int length = array.GetLength(1);
+    int width = array.GetLength(0);
+
+    for(int i = 0; i < width; i++)
+    {
+        for(int j = i; j < length; j++)
+        {
+            array[i, j] = digit;
+            digit++;
+        }
+
+        for(int k = i + 1, j = length - 1; k < width; k++)
+        {
+            if(array[k, j] == 0)
+            {
+                array[k, j] = digit;
+                digit++;
+            }
+            else return;
+        }
+        length--;
+
+        for(int k = width - 1, j = length - 1; j >= 0 + i; j--)
+        {
+            if(array[k, j] == 0)
+            {
+                array[k, j] = digit;
+                digit++;
+            }
+            else return;
+        }
+        width--;
+
+        for(int k = width - 1, j = 0 + i; k > i; k--)
+        {
+            if(array[k, j] == 0)
+            {
+                array[k, j] = digit;
+                digit++;
+            }
+            else return;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i, j] < 10) System.Console.Write(string.Format("{0, 5}", "0" + array[i, j] + " "));
+            else System.Console.Write(string.Format("{0, 5}", array[i, j] + " "));
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int rows = 4;
+int columns = 4;
+
+int[,] array = new int[rows, columns];
+
+FillArraySpirally(array);
+PrintArray(array);
